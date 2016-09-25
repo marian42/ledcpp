0a7b8c874ec34fbdfa00904fd8f1e7d25f2a1255
Marian <mail@marian42.de>
2016-09-25 21:10:09 +0000

    Update Swirl example

--- cpp/example/Swirl.cpp
+++ cpp/example/Swirl.cpp
@@ -6,11 +6,10 @@ protected:
 		float angle = atan2(x - 8.0, y - 8.0);
 		float radius = sqrt(pow(x - 8, 2) + pow(y - 8, 2));
 		float stretch = 2.0 + sin(time * 0.0001);
-		radius += angle * stretch;
 		
 		color.hsv(
-			int(time * 0.02 + radius * 0.5) % 255,
+			int(time * 0.02 - radius * 3.0) % 255,
 			255,
-			char(255.0 * (0.5 + 0.5 * sin(2.0 * radius / stretch - time * 0.006))));
+			char(255.0 * (0.5 + 0.5 * sin(2.0 * (radius + angle * stretch) / stretch - time * 0.006))));
 	}
 };
