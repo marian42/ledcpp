813a2b269b536cb0dd9a1e06c175aa868345ebd8
Marian <mail@marian42.de>
2016-09-26 00:41:33 +0000

    Another small update for the Swirl example

--- cpp/example/Swirl.cpp
+++ cpp/example/Swirl.cpp
@@ -5,7 +5,7 @@ protected:
 	void mainImage(Color& color, int x, int y) {
 		float angle = atan2(x - 8.0, y - 8.0);
 		float radius = sqrt(pow(x - 8, 2) + pow(y - 8, 2));
-		float stretch = 2.0 + sin(time * 0.0001);
+		float stretch = 2.5 + 0.5 * sin(time * 0.0001);
 		
 		color.hsv(
 			int(time * 0.02 - radius * 3.0) % 255,
